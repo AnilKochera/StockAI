897eb1f0f224b23ab75654b713188c360268b6f1
AnilKochera <98192680+AnilKochera@users.noreply.github.com>
2024-11-26 23:48:41 -0600

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
+    },
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+  define: {
+    'process.env': {
+      VITE_SERVER_PORT: 3001,
+    },
+  },
+});
